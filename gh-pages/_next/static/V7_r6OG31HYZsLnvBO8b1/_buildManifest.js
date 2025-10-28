9b624f45bf6754b3cfd85db3816ec7f6c33ff096
AryanMalhotra26 <malhotraryan15@gmail.com>
2025-10-28 14:00:08 -0400

    Deploy static export with .nojekyll

---
+++ gh-pages/_next/static/V7_r6OG31HYZsLnvBO8b1/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/golden-clipper//_next/:path+",
+        "destination": "/golden-clipper/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
